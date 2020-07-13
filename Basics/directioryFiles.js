2cc4f722870e52be6191579ead294b370dd45a2e
mmkvdev <madhuv299@gmail.com>
2020-07-13 20:41:06 +0530

    Update: Directory and File path of the current file

---
+++ Basics/directioryFiles.js
@@ -0,0 +1,8 @@
+/*
+    - printing out directories and files in nodejs.
+    - directories -> __dirname -> returns absolute path of the working directory
+    - files -> __filename -> returns the absolute path of the working directory along with the specified filename.
+*/
+
+console.log('directory', __dirname);
+console.log('filename', __filename);
