36d0d536f6df792595eac69d52eb523883d33ef7
mmkvdev <madhuv299@gmail.com>
2020-07-13 20:55:20 +0530

    Update: modules script

---
+++ ModulesandRequire/modules.js
@@ -0,0 +1,7 @@
+// const fighters = require('./kenganAshura');
+const { fighter, ages } = require('./kenganAshura');
+
+console.log(fighter);
+console.log(ages)
+//console.log(fighters.a);
+// console.log(fighter); // just because we import files using require doesn't mean that we can access the variables from the file.
