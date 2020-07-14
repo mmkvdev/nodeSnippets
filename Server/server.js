567e36d3fa229c0d6237597135b527c08c81a571
mmkvdev <madhuv299@gmail.com>
2020-07-14 21:24:46 +0530

    Update: Server File

---
+++ Server/server.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    // console.log('Request Made');
+    // console.log(req);
+    // console.log(req.url, req.method);
+
+
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+
+    // console.log(req.url);
+
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            // console.log(path);
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            //  console.log(path);
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            //  console.log(path);
+            break;
+    }
+
+    // send an HTML file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log('file not found');
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    });
+    // res.write('hello, coders');
+    // res.write('<h1>Hello</h1>');
+    // res.end();
+}); // future -> web sockets
+
+server.listen(3000, 'localhost', () => {
+    console.log('Listening for requests on the port number 3000');
+})
