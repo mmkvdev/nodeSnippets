8c938fd335b393b5e3c8a81e6fb0e5e4e6a431d7
mmkvdev <madhuv299@gmail.com>
2022-10-29 21:38:49 +0530

    node/first_api: building a sample api

--- 2.0/url/index.js
+++ 2.0/url/index.js
@@ -1,15 +1,36 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
+const productData = fs.readFileSync('../utils/data/meta_data.json', 'utf-8');
+const dataObj = JSON.parse(productData)
 
+// fs.readFile('../utils/data/meta_data.json', 'utf-8', (err, data) => {
+//     const productData = JSON.parse(data);
+// })
 // creating a web server
 const server = http.createServer((req, res) => {
     const pathName = req.url;
+    
     if (pathName === '/' || pathName === '/overview') {
         res.end('Hello from OVERVIEW routing web server !!!');
     } else if (pathName === '/product') {
         res.end('Hello from PRODUCT routing web server !!!');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.end(productData);
+        console.log(productData); 
+        // res.writeHead(200, {
+        //     // 'Content-type': 'text/html'
+        //     'Content-type': 'application/json'
+        // });
+        // res.end('<div style="background: white;">API</div>');
+        // res.end(data);
+        console.log(__dirname);
     }
+
 
     else {
         res.writeHead(404, 'fucking bad request', {
