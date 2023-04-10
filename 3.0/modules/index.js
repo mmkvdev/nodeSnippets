7f26e3e1e36b83395cc77e84282461188b2d3736
madhu varma <>
2023-04-10 16:03:57 +0000

    concept/modules: local modules

---
+++ 3.0/modules/index.js
@@ -0,0 +1,3 @@
+const sayHello = require('./local/index');
+
+console.log(sayHello('Naruto'));
---
+++ 3.0/modules/local/index.js
@@ -0,0 +1,5 @@
+function sayHello (userName) {
+    return `Hello ${userName} from Node Server`;
+}
+
+module.exports = sayHello;
