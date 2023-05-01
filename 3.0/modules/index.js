7e91c2e411f14f4edbd43191f8ee00b7c0c87897
mmkvdev <28980183+mmkvdev@users.noreply.github.com>
2023-05-01 15:06:48 +0000

    concept/module_export_reusability: reusability of a loaded module

--- 3.0/modules/index.js
+++ 3.0/modules/index.js
@@ -1,5 +1,10 @@
 const sayHello = require('./local/index');
 const add = require('./local/add');
 
+// loaded function can be reused any number of times
+const add1 = add(1,2);
+const add2 = add(3,4);
+
 console.log(sayHello('Naruto'));
-console.log(add(1,2));
+console.log(add1);
+console.log(add2);
